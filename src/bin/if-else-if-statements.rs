a3d294937d86a5e0ecb4be752c8bf7085bf5811c
Immaculate Munde <immaculatemunde1@gmail.com>
2026-05-06 23:18:39 +0300

    Task 3; using if-else-if ststements.

---
+++ src/bin/if-else-if-statements.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let my_number = 5; 
+
+    if my_number > 5 {
+        println!("greater than 5");
+    } else if my_number < 5 {
+        println!("less than 5");
+    } else {
+        println!("=5"); 
+   }
+}
