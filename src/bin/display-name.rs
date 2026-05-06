36bb79399fd4e2b8f5715e5e877638e67ea1e746
Immaculate Munde <immaculatemunde1@gmail.com>
2026-05-06 23:14:05 +0300

    Task 1a and 1b; display name function

---
+++ src/bin/display-name.rs
@@ -0,0 +1,11 @@
+fn display_first_name() {
+    println!("Immaculate"); 
+}
+fn display_last_name() {
+    println!("Munde");
+}
+
+fn main() {
+    display_first_name();
+    display_last_name();
+}
